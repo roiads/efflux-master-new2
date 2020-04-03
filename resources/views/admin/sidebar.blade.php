47ade265e7aa1260f7fee7d72cf6ffe7d033baee
nick tuttle <nickolas.tuttle@gmail.com>
2020-04-03 03:40:24 -0700

    Navbar Organize

--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -6,25 +6,20 @@
 <nav>
 <ul class="nav nav-pills nav-sidebar flex-column nav-flat" data-accordion="false" data-widget="treeview">
   @vLink(['title'=>'Dashboard','icon'=>'tachometer-alt fas','url'=>'/dashboard'])
-  @vLinks(['title'=>'Accounts','icon'=>'id-card fas'])
-    @vLink(['title'=>'Profiles','icon'=>'caret-right fas','url'=>'/profiles'])
-    @vLink(['title'=>'Accounts','icon'=>'caret-right fas','url'=>'/accounts'])
-    @if($isAdmin = auth()->user()->admin()) 
-      @vLink(['title'=>'Payment Methods','icon'=>'caret-right fas','url'=>'/payments']) 
-    @endif
-  @endvLinks
+  @vLink(['title'=>'Accounts','icon'=>'id-card fas','url'=>'/accounts'])
   @vLinks(['title'=>'Campaigns','icon'=>'bullhorn fas'])
     @vLink(['title'=>'Campaign Manager','icon'=>'caret-right fas','url'=>'/campaigns'])
     @vLink(['title'=>'Ad Manager','icon'=>'caret-right fas','url'=>'/ads'])
     @vLink(['title'=>'Waffles','icon'=>'stroopwafel fas','url'=>'/rules'])
   @endvLinks 
-  @vLinks(['title'=>'Content Manager','icon'=>'edit fas'])
+  @vLinks(['title'=>'Site Manager','icon'=>'edit fas'])
+    @vLink(['title'=>'Domains','icon'=>'caret-right fas','url'=>'/domains'])
+    @vLink(['title'=>'Routes','icon'=>'caret-right fas','url'=>'/routes'])    
     @vLink(['title'=>'Categories','icon'=>'caret-right fas','url'=>'/categories'])
     @vLink(['title'=>'Pages','icon'=>'caret-right fas','url'=>'/pages'])
     @vLink(['title'=>'Posts','icon'=>'caret-right fas','url'=>'/posts'])
     @vLink(['title'=>'Images','icon'=>'caret-right fas','url'=>'/images'])
   @endvLinks
-  @vLink(['title'=>'Domains & Routes','icon'=>'sitemap fas','url'=>'/domains'])
   @vLinks(['title'=>'Tracking','icon'=>'search-location fas'])
     @vLink(['title'=>'All Traffic','icon'=>'caret-right fas','url'=>'/traffic'])
     @vLinks(['title'=>'Traffic Sources','icon'=>'caret-right fas'])
@@ -34,15 +29,16 @@
     @endvLinks  
   @endvLinks  
   @vLinks(['title'=>'Reporting','icon'=>'chart-line fas'])
-    @if($isAdmin) 
-      @vLink(['title'=>'Revenue/Spend','icon'=>'caret-right fas','url'=>'/reports/revenue'])
-    @endif
     @vLink(['title'=>'Traffic Sources','icon'=>'caret-right fas','url'=>'/reports/traffic'])
     @vLink(['title'=>'Clicks','icon'=>'caret-right fas','url'=>'/reports/clicks'])
     @vLink(['title'=>'Leads','icon'=>'caret-right fas','url'=>'/reports/leads'])
   @endvLinks
-  @if($isAdmin) 
-    @vLink(['title'=>'Users & Permissions','icon'=>'users-cog fas','url'=>'/users'])
+  @if(auth()->user()->admin()) 
+    @vLinks(['title'=>'Admin','icon'=>'unlock fas'])
+      @vLink(['title'=>'Revenue/Spend','icon'=>'caret-right fas','url'=>'/reports/revenue'])
+      @vLink(['title'=>'Payment Methods','icon'=>'caret-right fas','url'=>'/payments']) 
+      @vLink(['title'=>'Users & Permissions','icon'=>'caret-right fas','url'=>'/users'])
+    @endvLinks
   @endif
 </ul>
 </nav>
