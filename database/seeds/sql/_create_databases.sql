8c14fa0ecdbe6ca0601f9ce30c5793f17f813a51
nick tuttle <nickolas.tuttle@gmail.com>
2020-04-06 14:34:08 -0700

    new efflux database full, with starting data

--- database/seeds/sql/_create_databases.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE `efflux`;
-CREATE DATABASE `efflux_accounts`;
-CREATE DATABASE `efflux_assets`;
-CREATE DATABASE `efflux_campaigns`;
-CREATE DATABASE `efflux_content`;
-CREATE DATABASE `efflux_tracker`;
-CREATE DATABASE `efflux_users`;
-CREATE DATABASE `efflux_workbots`;
--- database/seeds/sql/content_domains.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO `efflux_content`.`domains` (`name`, `id`, `path`) VALUES
-('getavio.com', 1, '/var/www/getavio.com/'),
-('sevenbars.com', 2, '/var/www/sevenbars.com/'),
-('sevenbars.test', 3, '/var/www/sevenbars.com/'),
-('localhost:8000', 4, ''),
-('getavio.test', 5, ''),
-('127.0.0.1:8000', 6, ''),
-('dev.sevenbars.com', 7, '');
