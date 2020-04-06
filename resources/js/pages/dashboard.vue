04a27014336fdef370c737b7894cb9bf7488d67c
nick tuttle <nickolas.tuttle@gmail.com>
2020-04-06 14:00:32 -0700

    css fixes,

--- resources/js/pages/dashboard.vue
+++ resources/js/pages/dashboard.vue
@@ -1,19 +1,53 @@
 <template>
-  <section class="content">
-    <div class="container-fluid">
-      <div class="row">
-        <b-card border-variant="success" align="center">
+  <div class="container-fluid">
+    <div>
+      <b-card-group deck>
+        <b-card border-variant="success" style="text-center" class="m-2">
           <b-card-text>Revenue</b-card-text>
           <b-card-text>Spend</b-card-text>
           <b-card-text>Total</b-card-text>
         </b-card>
-      </div>
-      <div class="row">
-        <accountTypeList @setId="accountTypeOverview"></accountTypeList>
-        <domainList @setId="domainOverview"></domainList>
-      </div>
+        <b-card border-variant="primary" style="text-center" class="m-2">
+          <b-card-text>Campaigns</b-card-text>
+          <b-card-text>Active</b-card-text>
+          <b-card-text>Complete</b-card-text>
+        </b-card>
+        <b-card border-variant="primary" style="text-center" class="m-2">
+          <b-card-text>Accounts</b-card-text>
+          <b-card-text>Active</b-card-text>
+          <b-card-text>Total</b-card-text>
+        </b-card>
+        <b-card border-variant="info" style="text-center" class="m-2">
+          <b-card-text>Sites</b-card-text>
+          <b-card-text>Active</b-card-text>
+          <b-card-text>Total</b-card-text>
+        </b-card>
+      </b-card-group>
     </div>
-  </section>
+    <div>
+      <b-card-group deck>
+        <b-card border-variant="info" style="text-center">
+          <b-card-text>Views</b-card-text>
+          <b-card-text>Clicks</b-card-text>
+          <b-card-text>Leads</b-card-text>
+        </b-card>
+        <b-card border-variant="info" style="text-center">
+          <b-card-text>Views</b-card-text>
+          <b-card-text>Clicks</b-card-text>
+          <b-card-text>Leads</b-card-text>
+        </b-card>
+        <b-card border-variant="danger" style="text-center">
+          <b-card-text>Warnings</b-card-text>
+          <b-card-text>Alerts</b-card-text>
+          <b-card-text>Notices</b-card-text>
+        </b-card>
+      </b-card-group>
+    </div>
+    <div class="row">
+      <accountTypeList @setId="accountTypeOverview"></accountTypeList>
+      <domainList @setId="domainOverview"></domainList>
+    </div>
+  </div>
 </template>
 <script>
 import accountTypeList from "./accounts/account/type/list";
