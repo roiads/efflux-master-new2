6c509fd0bcb601cc2616f31a043eb33aeea6a485
nick tuttle <nickolas.tuttle@gmail.com>
2020-04-23 22:23:23 -0700

    domain controller example

--- app/Http/Controllers/Assets/DomainCtrl.php
+++ app/Http/Controllers/Assets/DomainCtrl.php
@@ -2,25 +2,27 @@
 namespace App\Http\Controllers\Assets;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use \App\assets_domain as assets_domain;
+use \App\assets_domain as x;
 
 class DomainCtrl extends Controller {
  public function index() {
-  $Q = assets_domain::with('server')->where('status', '>', 0)->paginate(10);
+  $Q = x::with('server')->where('status', '>', 0)->paginate(10);
   return response()->json($Q);
  }
  public function search($args = null) {
-  $Q = assets_domain::with('server');
+  $Q = x::with('server');
   foreach ($args as $k => $v) {
    $Q = $Q->where($k, (key($v) ?? '='), ($v[key($v)] ?? $v));
   }
   $Q = $Q->get();
   return response()->json($Q);
  }
- public function show(assets_domain $domain, $id = null) {
-  return $domain->find($id);
+ public function show(x $x, $id = null) {
+  return $x->find($id);
  }
- public function create() {}
- public function update(Request $R, x $domain) {}
- public function destroy(assets_domain $domain) {}
+ public function create() {
+   return view('pages.v');
+ }
+ public function update(Request $R, x $x) {}
+ public function destroy(x $x) {}
 }
