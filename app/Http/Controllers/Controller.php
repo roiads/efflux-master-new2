ee6d5949326dcbecc92112be334ac91aacdf7bc7
nick tuttle <nickolas.tuttle@gmail.com>
2020-05-07 02:09:09 -0700

    minus controller parseagrs

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,43 +1,28 @@
 <?php
 namespace App\Http\Controllers;
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
-use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 class Controller extends BaseController {
 
- use AuthorizesRequests;
- use DispatchesJobs;
- use ValidatesRequests;
+    use AuthorizesRequests;
+    use DispatchesJobs;
+    use ValidatesRequests;
 
- public $errors = [];
+    public $errors = [];
 
- public function parseArgs(&$args) {
-  if (!empty($args)) {
-   $args = str_replace('/', '|', $args);
-   $args = explode('|', $args);
-   foreach ($args as $arg) {
-    if (stristr($arg, '=')) {
-     list($k, $v) = explode('=', $arg, 2);
-     $_[$k]       = $v;
-    } else {
-     $_[] = $arg;
+    public function anyErrors() {
+        if (empty($this->errors)) {
+            unset($this->errors);
+            return false;
+        }
+        $e = implode($this->errors);
+        return response($e, 412);
     }
-   }
-  }
-  return $_ ?? [];
- }
- public function anyErrors() {
-  if (empty($this->errors)) {
-   unset($this->errors);
-   return false;
-  }
-  $e = implode($this->errors);
-  return response($e, 412);
- }
- public function error($msg) {
-  $this->errors[] = $msg;
-  return false;
- }
+    public function error($msg) {
+        $this->errors[] = $msg;
+        return false;
+    }
 }
