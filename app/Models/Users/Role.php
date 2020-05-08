f6a501b8e1dd4819edc1ccc86e252a3e30cda1b2
Ronak Shah <coolronak.shah@rediffmail.com>
2020-05-08 15:00:36 +0530

    Update Add User

--- app/Models/Users/Role.php
+++ app/Models/Users/Role.php
@@ -1,17 +1,15 @@
 <?php
+
 namespace App\Models\Users;
 use App\Models\Models;
 
 class Role extends Models {
- public function __construct() {
-  $this->Table('users.roles');
-  $this->fillable = ['name', 'description'];
- }
- public function users() {
-  return $this->belongsToMany(
-   'App\Models\Users\User',
-   $this->Table('users.user_role'),
-   'role_id',
-   'user_id');
- }
+
+	protected $connection = 'efflux_users';
+
+	protected $fillable = ['name', 'description'];
+	
+	public function users() {
+		return $this->belongsToMany('App\Models\Users\User', 'user_role', 'role_id', 'user_id');
+	}	
 }
