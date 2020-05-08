47d8bf321b130dc5f10becb79957ed13fbb6354e
Ronak Shah <coolronak.shah@rediffmail.com>
2020-05-08 11:07:17 +0530

    Add User Debug

---
+++ resources/views/components/users/add-user-modal.vue
@@ -0,0 +1,42 @@
+<template>
+	<div>
+		<b-form @submit="onSubmit" @reset="onReset" v-if="show">
+			<b-form-group id="input-group-first-name" label="First Name:" label-for="input-first-name">
+				<b-form-input id="input-first-name" v-model="newUser.first_name" required></b-form-input>
+			</b-form-group>
+
+			<b-form-group id="input-group-last-name" label="Last Name:" label-for="input-last-name">
+				<b-form-input id="input-last-name" v-model="newUser.last_name" required></b-form-input>
+			</b-form-group>
+
+			<b-form-group id="input-group-email" label="Email:" label-for="input-email">
+				<b-form-input id="input-email" type="email" v-model="newUser.email" required></b-form-input>
+			</b-form-group>
+
+		</b-form>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: "add-user-modal",
+		data() {
+			return {
+				newUser: {
+					first_name: '',
+					last_name: '',
+					email: '',
+					password: '',
+					roles: []
+				}
+			}
+		},
+		mounted() {
+
+		},
+		methods: {
+
+		}
+
+	}
+</script>
--- resources/views/pages/users.vue
+++ resources/views/pages/users.vue
@@ -1,32 +1,41 @@
 <template>
   <div>
     <page-header :crumbs="crumbs">
-      <b-btn variant="success" @click="user-create">Add User</b-btn>
+      <b-btn variant="success" @click="addUser">Add User</b-btn>
       <b-btn v-if="user.id" @click="user-edit">Edit {{user.id}}</b-btn>
     </page-header>
     <page-body>
       <user-index :users="users" />
+      
+      <b-modal no-stacking id="add-user-modal" size="lg" title="Add New User">
+        <add-user-modal></add-user-modal>
+      </b-modal>
+
     </page-body>
   </div>
 </template>
+
 <script>
-export default {
-  name: "users-page",
-  data: () => ({
-    crumbs: [["user manager", "/users"]],
-    users: [],
-    user: []
-  }),
-  mounted() {
-    this.getUsers();
-  },
-  methods: {
-    getUsers() {
-      axios.get(`/api/user`).then(({ data }) => (this.users = data));
+  export default {
+    name: "users-page",
+    data: () => ({
+      crumbs: [["user manager", "/users"]],
+      users: [],
+      user: []
+    }),
+    mounted() {
+      this.getUsers();
     },
-    getUser(id) {
-      axios.get(`/api/user${id}`).then(({ data }) => (this.user = data));
+    methods: {
+      getUsers() {
+        axios.get(`/api/user`).then(({ data }) => (this.users = data));
+      },
+      getUser(id) {
+        axios.get(`/api/user${id}`).then(({ data }) => (this.user = data));
+      },
+      addUser() {
+        this.$bvModal.show("add-user-modal");
+      }
     }
-  }
-};
+  };
 </script>
