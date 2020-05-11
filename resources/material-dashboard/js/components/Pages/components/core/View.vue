771c362707124f99e3fa1b0a7d60a47c278eb8c8
Ronak Shah <coolronak.shah@rediffmail.com>
2020-05-11 14:26:07 +0530

    Update Theme Templates

--- resources/material-dashboard/js/components/Pages/components/core/View.vue
+++ resources/material-dashboard/js/components/Pages/components/core/View.vue
@@ -3,23 +3,13 @@
     id="pages"
     :class="$vuetify.theme.dark ? undefined : 'grey lighten-3'"
   >
-    <v-img
-      :class="{
-        'v-image--sm': this.$vuetify.breakpoint.smAndDown,
-        'v-image--md': this.$vuetify.breakpoint.mdAndDown
-      }"
-      :src="require(`../../../../assets/${src || 'clint-mckoy.jpg'}`)"
-      gradient="to top, rgba(0, 0, 0, .5), rgba(0, 0, 0, .5)"
-      min-height="100%"
-    >
-      <v-responsive
+    <v-responsive
         :style="styles"
         min-height="100vh"
         class="d-flex align-center"
       >
         <router-view />
       </v-responsive>
-    </v-img>
   </v-content>
 </template>
 
