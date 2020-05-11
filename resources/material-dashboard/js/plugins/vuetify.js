995cc3b56664013c57a3cbc0ac14fefcecc9dd27
Ronak Shah <coolronak.shah@rediffmail.com>
2020-05-11 09:46:58 +0530

    Update Changes

--- resources/material-dashboard/js/plugins/vuetify.js
+++ resources/material-dashboard/js/plugins/vuetify.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
-import Vuetify from 'vuetify/lib'
+//import Vuetify from 'vuetify/lib'
+import Vuetify from 'vuetify';
 import i18n from '../i18n'
 import '../../sass/overrides.sass'
 
