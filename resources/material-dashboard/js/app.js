630a77f656d334492e1cb3cf28265cbbf4d664a5
Ronak Shah <coolronak.shah@rediffmail.com>
2020-05-11 09:53:53 +0530

    Update Changes

--- resources/material-dashboard/js/app.js
+++ resources/material-dashboard/js/app.js
@@ -13,14 +13,13 @@ import './plugins/base'
 import './plugins/chartist'
 import './plugins/vee-validate'
 import './plugins/vue-world-map'
-import vuetify from './plugins/vuetify'
+import './plugins/vuetify'
 import i18n from './i18n'
 
 
 const app = new Vue({
   router,
   store,
-  vuetify,
   i18n,
   render: h => h(App),
 }).$mount('#app')
